b3cd3d95057e7087952d355d71f639d6bc2ffd2c
Awais <awaisasif096@gmail.com>
2025-03-12 03:05:00 +0500

    Added render.yaml for deployment

--- app.ts
+++ app.ts
@@ -1,21 +1,4 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import videoRoutes from "./routes/video.routes";
 
-// dotenv.config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/videos", videoRoutes);
-
-// export default app;
-
-
-//deepseek
 
 import express from "express";
 import cors from "cors";
